99acaa38cc73aade70cadaea51ed4bc29bd22378
ryan6921 <rmkaw@asu.edu>
2019-09-27 21:05:24 -0500

    added html wireframe, updating value by state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+constructor(props){
+  super(props);
+  this.state = {
+    value: 'lookup a tool my guy'
+  }
+  
+  this.handleChange = this.handleChange.bind(this);
+}
+
+handleChange(){
+  
+}
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+ 
+    <body>
+      <nav role="navigation">Nav <span class="login" role="login">Sign in</span></nav>
+      <section>
+      <header role="banner">
+         <h1 role="title">Tool Library?</h1>
+         <h3>Channel your inner frugal handyman by BORROWING instead of BUYING your tools!!!!!</h3>
+         <h3>Start here:</h3>
+         <input type='search' value={this.state.value} onChange={this.handleChange} role="searchbar"></input>
+         <input type='submit' value='submit' role="submit-button"></input>
       </header>
-    </div>
-  );
+      </section>
+    
+      <section role="landing-page">
+        <h3>Here are some default pictures of tools: </h3>
+        <img src="https://cdn.shoplightspeed.com/shops/618849/files/9043966/800x1024x2/schylling-classic-rubber-duck.jpg" height="400" width="400"/>
+      </section>
+      <section>
+        <p>Tool Library allows you to look up the tools you need for your next big project without the hassle of buying things you only need once! ISN'T THAT GREAT?!?</p>
+        <p>Reserve your tools online, and come in-store for pickup when you're ready! No hassle guaranteed!</p>
+      </section>
+      <footer role="contact-info">Footer</footer>
+    </body>
+    );
+  }
 }
 
 export default App;
