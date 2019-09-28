ea5670d982336e25efc051657708f27223820728
Brandon El Haj <brandon.elhaj@gmail.com>
2019-09-28 13:57:26 -0700

    Updated

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { Router } from 'react-router-dom';
 import './index.css';
 import AppRouter from './components/AppRouter/AppRouter'
 import history from './history'
-import App from './App';
-
 
 ReactDOM.render(
     <Router history={history}>
