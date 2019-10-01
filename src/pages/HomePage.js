430efc2bb5d2101b6bd360ff055e36a4a80ac6f0
computershawn <shontek@gmail.com>
2019-10-01 00:44:02 -0700

    Search box now filters tool cards

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,13 +4,21 @@ import AutoCompleteWrap from '../components/AutoComplete/AutoCompleteWrap'
 import '../assets/css/HomePage.css'
 
 class HomePage extends Component {
+    state = {
+        toolIdListFiltered: []
+    }
+
+    updateToolIDs(toolIDs) {
+        this.setState({
+            toolIdListFiltered: toolIDs
+        })
+    }
 
     render() {
-        console.log(this.props)
         return (
             <div className="HomePage">
-                    <AutoCompleteWrap />
-                    <ToolCardList />
+                <AutoCompleteWrap sendUpToolIDs={(ids) => this.updateToolIDs(ids)} />
+                <ToolCardList toolIdListFiltered={this.state.toolIdListFiltered} />
             </div>
         )
     }
