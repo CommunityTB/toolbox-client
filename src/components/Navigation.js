6ced9612d37de300e6183dd089ddf01a9b47d0f1
Brandon El Haj <brandon.elhaj@gmail.com>
2019-09-30 22:12:52 -0700

    Building Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,32 +1,59 @@
-// import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
-// import '../assets/css/styles.css';
-// import logo from '../assets/images/CT_logo_color.svg';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/css/styles.css';
+import logo from '../assets/images/CT_logo_color.svg';
 
-// export default class Navigation extends Component {
+export default class Navigation extends Component {
 
-//     handleUserLogout = () => {
+    handleUserLogout = () => {
 
-//     }
+    }
 
-//     renderLoginLink = () => {
-//         return (
+    renderLoginLinks = () => {
+        return (
+            <div className='Nav_not_logged_in'>
+                <Link
+                    to='/login'>
+                    Login    
+                </Link>
+                <Link 
+                    to='/register'>
+                    Register
+                </Link>
+                <Link 
+                    to='/inventory'>
+                    Inventory    
+                </Link>
+                <Link
+                    to='/find_us'>
+                    Find Us    
+                </Link>
+                <Link 
+                    to='/about'>
+                    About
+                </Link>
+            </div>
+        )
+    }
 
-//         )
-//     }
+    renderLogoutLinks = () => {
+        return (
+            <div className='Nav_not_logged_in'>
+                <Link to='/'
+                        onClick={this.handleUserLogout}
+                        >
+                        Logout
+                </Link>
+            </div>
+        )
+    }
 
-//     renderLogoutLink = () => {
-//         return (
+    render() {
+        return (
+            <nav className ='Nav'>
+                <div><Link to='/'></Link></div>
+            </nav>
+        )
+    }
 
-//         )
-//     }
-
-//     render() {
-//         return (
-//             <nav className ='Nav'>
-//                 <div><Link to='/'></Link></div>
-//             </nav>
-//         )
-//     }
-
-// }
+}
