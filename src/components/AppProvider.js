4e75f0e1806f4f30205819256486c0f3a72716b5
computershawn <shontek@gmail.com>
2019-10-01 21:05:23 -0700

    Create form and logic for user registration

--- src/components/AppProvider.js
+++ src/components/AppProvider.js
@@ -4,54 +4,64 @@ import { API_BASE_URL } from '../config';
 export const AppContext = React.createContext();
 
 class AppProvider extends Component {
-    state = {
-      error: null,
-      tools: [],
-    }
+  state = {
+    error: null,
+    tools: [],
+  }
 
-    componentDidMount = () => {
-      fetch(`${API_BASE_URL}/tools`, {
-        method: 'GET',
-        // headers: {
-        //   'content-type': 'application/json',
-        //   'Authorization': `Bearer ${config.API_KEY}`
-        // }
+  componentDidMount = () => {
+    fetch(`${API_BASE_URL}/tools`, {
+      method: 'GET',
+      // headers: {
+      //   'content-type': 'application/json',
+      //   'Authorization': `Bearer ${config.API_KEY}`
+      // }
+    })
+      .then((toolsResponse) => {
+        if (!toolsResponse.ok) {
+          return toolsResponse.json().then(error => Promise.reject(error))
+        }
+        return toolsResponse.json()
       })
-        .then((toolsResponse) => {
-          if(!toolsResponse.ok) {
-            return toolsResponse.json().then(error => Promise.reject(error))
-          }
-          return toolsResponse.json()
-        })
-        .then((tools) => {
-          this.setState({tools})
-        })
-        .catch(err => {
-          this.setState({
-            error: err.message
-          });
-        })
-    }
+      .then((tools) => {
+        this.setState({ tools })
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      })
+  }
 
-    render() {
-        return (
-            <AppContext.Provider
-                value = {{
-                    state: {
-                        ...this.state
-                        // passes state to other components
-                    },
-                    actions: {
-                      reserveTool: (toolId) => {
-                        alert(`This should reserve tool ${toolId}. TODO: Update AppProvider.js to make the API call.`)
-                      },
-                      
-                    },
-                }}>    
-                {this.props.children}    
-            </AppContext.Provider>
-        )
-    }
+  render() {
+    return (
+      <AppContext.Provider
+        value={{
+          state: {
+            ...this.state
+            // passes state to other components
+          },
+          actions: {
+            reserveTool: (toolId) => {
+              alert(`This should reserve tool ${toolId}. TODO: Update AppProvider.js to make the API call.`)
+            },
+            registerUser: (newUser) => {
+              console.log('TODO: Create a new user with these properties:\n', newUser)
+              /* POST to the API /users endpoint */
+              /*
+                user_name TEXT NOT NULL UNIQUE,
+                user_password TEXT NOT NULL,
+                email TEXT NOT NULL,
+                first_name TEXT NOT NULL,
+                last_name TEXT NOT NULL,
+              */
+            }
+          },
+        }}>
+        {this.props.children}
+      </AppContext.Provider>
+    )
+  }
 }
 
 export default AppProvider
