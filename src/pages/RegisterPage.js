d02b65e2fba31da358755c776602480e5fb5f77b
computershawn <shontek@gmail.com>
2019-10-01 19:21:11 -0700

    Background image on details page, other minor changes

---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class RegisterPage extends Component {
+
+    state = {
+    }
+
+    render() {
+        return (
+          <p>PUT REGISTRATION FORM HERE</p>
+        );
+    }
+}
+
+export default RegisterPage;
