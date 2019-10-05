cb6edc4c429a735b015425f8399c061f4cc413a7
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-10-04 22:19:44 -0700

    updated to zeit url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || `http://localhost:8000/api`,
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || `https://toolbox-client-n3d00jstx.now.sh//api`,
   REACT_APP_TOKEN_KEY: 'community-toolbox-auth-token',
 }
