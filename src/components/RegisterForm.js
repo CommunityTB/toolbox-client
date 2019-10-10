1e86010c00ae5b9dc4be3701d61563e6800d158f
Brandon El Haj <brandon.elhaj@gmail.com>
2019-10-09 21:41:16 -0700

    Login and Register

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { AppContext } from '../components/AppProvider';
-
+import { Button, Input } from '../helpers/Helpers';
 
 class RegisterForm extends Component {
   constructor(props) {
@@ -11,7 +11,8 @@ class RegisterForm extends Component {
       last_name: `Last Name`,
       user_password: `********`,
       email: `your-email@example.com`,
-      user_name: `your-username`
+      user_name: `your-username`,
+      // error: null
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -41,35 +42,77 @@ class RegisterForm extends Component {
   render() {
     const { first_name, last_name, user_name, user_password, email } = this.state
     return (
-      <form onSubmit={this.handleSubmit}>
-      <div className="form-section">
-        <div className="label-holder"><label htmlFor="first-name">First Name</label></div>
-        <input className="form-input-field" id="first-name" type="text" name="firstName" placeholder={first_name} onChange={this.handleChange} required />
-      </div>
-
-      <div className="form-section">
-        <div className="label-holder"><label htmlFor="last-name">Last Name</label></div>
-        <input className="form-input-field" id="last-name" type="text" name="lastName" placeholder={last_name} onChange={this.handleChange} required />
-      </div>
-
-      <div className="form-section">
-        <div className="label-holder"><label htmlFor="email">Email</label></div>
-        <input className="form-input-field" id="email" type="text" name="email" placeholder={email} onChange={this.handleChange} required />
-      </div>
-
-      <div className="form-section">
-        <div className="label-holder"><label htmlFor="user-name">Username</label></div>
-        <input className="form-input-field" id="user-name" type="text" name="userName" placeholder={user_name} onChange={this.handleChange} required />
-      </div>
-
-      <div className="form-section">
-        <div><label htmlFor="password">Password</label></div>
-        <input className="form-input-field" id="password" type="password" name="password" placeholder={user_password} onChange={this.handleChange} required />
-      </div>
-
-      <button type="submit">Submit</button>
-      <Link className="cancel-action" to='/'>Cancel</Link>
-    </form>
+      <div className='form-space'>
+        <form onSubmit={this.handleSubmit} className="register-form">
+          
+            <legend className='page-title'>Register</legend> 
+            <label htmlFor="first-name" className="first-name">First Name</label>
+            <Input 
+              className="form-input-field" 
+              id="first-name" 
+              type="text" 
+              name="firstName" 
+              // placeholder={first_name} 
+              onChange={this.handleChange} 
+              required 
+            />
+        
+            <label htmlFor="last-name" className="last-name">Last Name</label>
+            <Input 
+              className="form-input-field" 
+              id="last-name" 
+              type="text" 
+              name="lastName" 
+              // placeholder={last_name} 
+              onChange={this.handleChange} 
+              required 
+            />
+        
+            <label htmlFor="email" className="email">Email</label>
+            <Input 
+              className="form-input-field" 
+              id="email" 
+              type="text" 
+              name="email" 
+              // placeholder={email} 
+              onChange={this.handleChange} 
+              required 
+            />
+          
+            <label htmlFor="user-name" className="user-name">Username</label>
+            <Input 
+              className="form-input-field" 
+              id="user-name" 
+              type="text" 
+              name="userName" 
+              // placeholder={user_name} 
+              onChange={this.handleChange} 
+              required 
+            />
+          
+            <label htmlFor="password" className="user-password">Password</label>
+            <Input 
+              className="form-input-field" 
+              id="password" 
+              type="password" 
+              name="password" 
+              // placeholder={user_password} 
+              onChange={this.handleChange} 
+              required 
+            />
+          
+            <Button type="submit" className="register-button">
+              Submit
+            </Button>
+            
+            <Link className="cancel-action" to='/'>
+              Cancel
+            </Link>
+        {/* <div role='alert'>
+          {error && <p className='red-error'>{error}</p>}
+        </div> */}
+      </form>
+    </div>
     )
   }
 }
