ee95f950618bc480ed095535fe4f35acaa28af93
Brandon El Haj <brandon.elhaj@gmail.com>
2019-10-10 21:58:24 -0700

    removed tests

--- src/tests/AppProvider.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import AppProvider from '../components/AppProvider';
-import ReactDOM from 'react-dom';
-
-it('AppProvider renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<AppProvider/>, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
--- src/tests/LoginForm.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import LoginForm from '../components/Login/LoginForm';
-import ReactDOM from 'react-dom';
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<LoginForm/>, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
