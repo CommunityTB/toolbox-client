832e1896b17fb628c8da69051060116a684c2b4b
Brandon El Haj <brandon.elhaj@gmail.com>
2019-10-14 22:55:27 -0700

    Updated CSS and removed files

--- src/components/BadgeCounter/BadgeCounter.js
+++ src/components/BadgeCounter/BadgeCounter.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './BadgeCounter.css';
 
 export default function BadgeCounter(props){
   const badge = props.count ? <div className="tools_count">{props.count}</div> : null;
